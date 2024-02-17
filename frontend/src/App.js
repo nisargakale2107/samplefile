7efd1d9f39e263831da45387d17c1abd931b4ebc
nisargakale <nskale_b22@ce.vjti.ac.in>
2024-02-17 12:52:21 +0530

    404page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NotFound from "./pages/404notfound";
 
+function Home() {
+  return <h1>Home Page</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" component={Home} />
+        {/* Other routes */}
+        <Route exact path="*" element={<NotFound/>}/>
+      </Routes>
+    </Router>
   );
 }
 
