34ce07f66c5e08495ff84665ce29f6c599fd2660
Aastha2675 <aasthamhatre2607@gmail.com>
2024-02-17 19:06:48 +0530

    added navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import "../css/navbar.css";
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <nav className="nav-container ">
+        <div className="website-name">Website Name</div>
+        <div className="other">
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
 
-export default Navbar
+            <li>
+              <a href="#aboutus">About us</a>
+            </li>
+
+            <li>
+              <button>Become Artist</button>
+            </li>
+
+            <li>
+              <button>Profile</button>
+            </li>
+
+            <li> 
+              Conect Wallet
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
